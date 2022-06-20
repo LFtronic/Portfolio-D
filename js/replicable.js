adc4a6d0626355a6b44d189ccd8120586961336e
LFtronic <a01023675@itesm.mx>
2022-06-20 14:48:48 -0500

    first navigation iteration

--- js/replicable.js
+++ js/replicable.js
@@ -1,3 +1,18 @@
 $.getJSON("./json/replicable.json", function(json) {
-    console.log(json); 
+    var nav = json.nav;
+    var navBar = $("nav");
+    var navItems = [];
+    for (var item in nav) {
+        navItems.push(nav[item]);
+    };
+    navItems.forEach(item => {
+        var e = document.createElement("a");
+        e.innerHTML = item["name"];
+        e.href = item["href"];
+        e.id = item["id"];
+        e.classList = item["classes"];
+        e.attributes = item["attributes"];
+        e.target = item["target"];
+        navBar.append(e)
+    });
 });
