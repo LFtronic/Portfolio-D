ea721407a163e01f0d27a9216f6cfe0bc12371a0
LFtronic <a01023675@itesm.mx>
2022-06-20 22:11:31 -0500

    change in htaccess and replicable

--- js/replicable.js
+++ js/replicable.js
@@ -1,4 +1,16 @@
-$.getJSON("./json/replicable.json", function(json) {
+window.onload = function() {
+    $.getJSON("./json/replicable.json", function(json) {
+    var logo = json.logo;
+    var logoAnchor = $("#header-bar-logo");
+    var logoImage = document.createElement("img");
+    logoImage.src = logo["src"];
+    logoImage.alt = logo["alt"];
+    logoAnchor.append(logoImage);
+    var head = json.head;
+    var headContainer = $("head");
+    head.forEach(item => {
+        headContainer.append(item);
+    });
     var nav = json.nav;
     var navBar = $("nav");
     var navItems = [];
@@ -11,8 +23,13 @@ $.getJSON("./json/replicable.json", function(json) {
         e.href = item["href"];
         e.id = item["id"];
         e.classList = item["classes"];
+        var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname;
+        if (e.href == currentURL) {
+            e.classList.add("active");
+        }
         e.attributes = item["attributes"];
         e.target = item["target"];
         navBar.append(e)
     });
 });
+  };
