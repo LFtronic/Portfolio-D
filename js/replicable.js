4384ffee65a812f35392e2dc9353799426e36062
LFtronic <a01023675@itesm.mx>
2022-06-22 17:50:37 -0500

    fix

--- js/replicable.js
+++ js/replicable.js
@@ -1,37 +1,36 @@
 window.onload = function() {
     $.getJSON("./json/replicable.json", function(json) {
-    var logo = json.logo;
-    var logoAnchor = $("#header-bar-logo");
-    var logoImage = document.createElement("img");
-    logoImage.src = logo["src"];
-    logoImage.alt = logo["alt"];
-    logoAnchor.append(logoImage);
-    var head = json.head;
-    var headContainer = $("head");
-    head.forEach(item => {
-        headContainer.append(item);
+        var logo = json.logo;
+        var logoAnchor = $("#header-bar-logo");
+        var logoImage = document.createElement("img");
+        logoImage.src = logo["src"];
+        logoImage.alt = logo["alt"];
+        logoAnchor.append(logoImage);
+        var head = json.head;
+        var headContainer = $("head");
+        head.forEach(item => {
+            headContainer.append(item);
+        });
+        var nav = json.nav;
+        var navBar = $("nav");
+        var navItems = [];
+        for (var item in nav) {
+            navItems.push(nav[item]);
+        };
+        navItems.forEach(item => {
+            var e = document.createElement("a");
+            e.innerHTML = item["name"];
+            e.href = window.location.protocol + "//" + window.location.host + window.location.pathname + item["href"];
+            e.id = item["id"];
+            e.classList = item["classes"];
+            var currentURL = window.location.href;
+            if (e.href == currentURL) {
+                e.classList.add("active");
+            }
+            e.onclick = item["onclick"];
+            e.attributes = item["attributes"];
+            e.target = item["target"];
+            navBar.append(e)
+        });
     });
-    var nav = json.nav;
-    var navBar = $("nav");
-    var navItems = [];
-    for (var item in nav) {
-        navItems.push(nav[item]);
-    };
-    navItems.forEach(item => {
-        var e = document.createElement("a");
-        e.innerHTML = item["name"];
-        e.href = window.location.protocol + "//" + window.location.host + item["href"];
-        e.id = item["id"];
-        e.classList = item["classes"];
-        var currentURL = window.location.href;
-        console.log(currentURL)
-        if (e.href == currentURL) {
-            e.classList.add("active");
-        }
-        e.onclick = item["onclick"];
-        e.attributes = item["attributes"];
-        e.target = item["target"];
-        navBar.append(e)
-    });
-});
-  };
+};
